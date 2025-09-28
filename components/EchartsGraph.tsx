7487e0b6274e17767d77a78dda012e0b93f9ded2
Er Xuanhe <exhere519@gmail.com>
2025-09-28 16:38:20 +0800

    Feat: Add custom toolbox controls and styling

--- components/EchartsGraph.tsx
+++ components/EchartsGraph.tsx
@@ -50,6 +50,54 @@ const EchartsGraph: React.FC<EchartsGraphProps> = ({ data, highlightedNodeId, on
 
   const option = {
     tooltip: {},
+    toolbox: {
+      show: true,
+      orient: 'vertical',
+      left: 'right',
+      top: 'center',
+      feature: {
+        myZoomIn: {
+          show: true,
+          title: 'Zoom In',
+          icon: 'path://M10.5,0 C16.3,0 21,4.7 21,10.5 C21,16.3 16.3,21 10.5,21 C4.7,21 0,16.3 0,10.5 C0,4.7 4.7,0 10.5,0 Z M10.5,2 C5.8,2 2,5.8 2,10.5 C2,15.2 5.8,19 10.5,19 C15.2,19 19,15.2 19,10.5 C19,5.8 15.2,2 10.5,2 Z M10,5 L11,5 L11,10 L16,10 L16,11 L11,11 L11,16 L10,16 L10,11 L5,11 L5,10 L10,10 L10,5 Z',
+          onclick: () => {
+            const instance = echartRef.current?.getEchartsInstance();
+            if (instance) {
+              const option = instance.getOption();
+              const zoom = option.series[0].zoom * 1.2;
+              instance.setOption({ series: [{ ...option.series[0], zoom }] });
+            }
+          },
+        },
+        myZoomOut: {
+          show: true,
+          title: 'Zoom Out',
+          icon: 'path://M10.5,0 C16.3,0 21,4.7 21,10.5 C21,16.3 16.3,21 10.5,21 C4.7,21 0,16.3 0,10.5 C0,4.7 4.7,0 10.5,0 Z M10.5,2 C5.8,2 2,5.8 2,10.5 C2,15.2 5.8,19 10.5,19 C15.2,19 19,15.2 19,10.5 C19,5.8 15.2,2 10.5,2 Z M5,10 L16,10 L16,11 L5,11 L5,10 Z',
+          onclick: () => {
+            const instance = echartRef.current?.getEchartsInstance();
+            if (instance) {
+              const option = instance.getOption();
+              const zoom = option.series[0].zoom / 1.2;
+              instance.setOption({ series: [{ ...option.series[0], zoom }] });
+            }
+          },
+        },
+        restore: {
+          show: true,
+          title: 'Reset',
+        },
+        saveAsImage: { show: true },
+      },
+      iconStyle: {
+        borderColor: '#666',
+        borderWidth: 1,
+      },
+      emphasis: {
+        iconStyle: {
+          borderColor: '#3e98ff',
+        },
+      },
+    },
     legend: [{
       data: data.categories.map(a => a.name)
     }],
